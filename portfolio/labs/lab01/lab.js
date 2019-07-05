11a1bb6a794f9e0d363df9e7d7b368d5cb755639
tigerwu0726 <52515351+tigerwu0726@users.noreply.github.com>
2019-07-05 17:51:58 +0800

    ip2019s

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,9 +4,42 @@
 
 
 function start(e) {
-
+var name = [
+{
+    char:'吳',
+   big5: 'A764',
+   cns:'1-4946',
+   unicode:'5433'
+},
+{
+    char:'宗',
+   big5: 'A976',
+   cns:'1-4C78',
+   unicode:'5B97'
+},
+{
+    char:'遠',
+   big5: 'BBB7',
+   cns:'1-6B3F',
+   unicode:'9060'
 }
 
+];
+
+ var ele = document.getElementById("div1")
+
+ for (var i = 0; i< 3 ; i++) {
+    ele.innerHTML += name[i].char;
+    ele.innerHTML +='big5=';
+    ele.innerHTML += name[i].big5;
+    ele.innerHTML +='cns=';
+    ele.innerHTML += name[i].cns;
+    ele.innerHTML +='unicode=';
+    ele.innerHTML += name[i].unicode;
+    ele.innerHTML +='<br>';
+
+ }
+}
 /*
 https://www.w3schools.com/jsref/met_document_addeventlistener.asp
 
--- portfolio/labs/lab02/lab.js
+++ portfolio/labs/lab02/lab.js
@@ -1,10 +1,24 @@
 /*
 
 */
-
+function update(){
+    var x = document.getElementById('in1').value;
+    var ele =document.getElementById('info1');
+    var y ='';
+    for (var i = 0; i < x.length; i++) {
+       y+= x.charAt(i)
+       y+='U+';
+       y+=x.charCodeAt(i).toString(16).toUpperCase();
+       y+='<br>';
+    }
+    ele.innerHTML=y;
+    document.getElementById('info3').innerHTML=
+    document.getElementById('in1').value.replace('韓國瑜','是智障')
+}
 
 function start(e) {
-
+document.getElementById('in1').addEventListener("input", update);
+  update();
 }
 
 /*
